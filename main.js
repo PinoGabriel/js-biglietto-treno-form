a9a99cfb894174eb3571ef4f77d52021fa715407
PinoGabriel <pinogabriel01@gmail.com>
2023-10-27 16:43:58 +0200

    nothing change

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 let x = document.getElementById("user").value;
 let y =  document.getElementById("km").value;
 let z =  document.getElementById("eta").value;
+let myBtn = document.getElementById("myBtn").value;
 
-console.log("x", user);
-console.log("y", km);
-console.log("z", eta);
+console.log("user", x);
+console.log("km", y);
+console.log("età", z);
 
 document.getElementById("costoBiglietto").innerHTML = x;
+
