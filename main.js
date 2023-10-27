a1115c92f02eef4b2b633ea08b5a000767212006
PinoGabriel <pinogabriel01@gmail.com>
2023-10-27 17:37:43 +0200

    trying

--- main.js
+++ main.js
@@ -3,11 +3,25 @@ let btn = document.getElementById("myBtn");
 let user = document.getElementById("user");
 let chilo =  document.getElementById("km");
 let age =  document.getElementById("eta");
+let costokm = 0.21
+let prezzo = ""
 
 btn.addEventListener("click", function() {
-    alert("L'utente ha scritto: " + user.value)
-    alert("L'utente ha scritto: " + chilo.value)
-    alert("L'utente ha scelto: " + age.value)
+    console.log("L'utente ha scritto: " + user.value)
+    console.log("L'utente ha scritto: " + chilo.value)
+    console.log("L'utente ha scritto: " + age.value);
+    if (age = "minorenne") {
+        prezzo = chilo * costokm;
+        prezzo = prezzo - (20/100 * prezzo);
+        document.getElementById("costoBiglietto").innerHTML = `Ridotto del 20%, il prezzo è di ${prezzo}`;
+    } else if (age = "over65") {
+        prezzo = chilo * costokm;
+        prezzo = prezzo - (40/100 * prezzo);
+        document.getElementById("costoBiglietto").innerHTML = `Ridotto del 40%, il prezzo è di ${prezzo}`;
+    } else {
+        prezzo = chilo * costokm;
+    }
 });
 
-console.log("user", user);
+
+document.getElementById("costoBiglietto").innerHTML = `Prezzo intero, il costo del biglietto è di ${prezzo}`;
