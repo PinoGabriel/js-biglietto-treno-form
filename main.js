7eef11e4290489878cd4fb1df73232de28bc94d4
PinoGabriel <pinogabriel01@gmail.com>
2023-10-27 16:36:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let x = document.getElementById("user").value;
+let y =  document.getElementById("km").value;
+let z =  document.getElementById("eta").value;
+
+console.log("x", user);
+console.log("y", km);
+console.log("z", eta);
+
+document.getElementById("costoBiglietto").innerHTML = x;
