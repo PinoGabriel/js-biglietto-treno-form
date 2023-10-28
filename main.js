dc7de960b18fc5c438802bddde38b9fa470679d6
PinoGabriel <pinogabriel01@gmail.com>
2023-10-28 16:51:45 +0200

    almost finished

--- main.js
+++ main.js
@@ -5,24 +5,30 @@ let chilo =  document.getElementById("km");
 let age =  document.getElementById("eta");
 let costokm = 0.21
 let prezzo;
+let costofinale
+let myName = document.getElementById("myName");
+let costoBiglietto = document.getElementById("costoBiglietto");
 
 
 btn.addEventListener("click", function() {
     console.log("L'utente ha scritto: " + user.value)
     console.log("L'utente ha scritto: " + chilo.value)
     console.log("L'utente ha scritto: " + age.value);
-    if (age.value=="2") {
-        prezzo = chilo * costokm;
-        prezzo = prezzo - (20/100 * prezzo);
-        document.getElementById("costoBiglietto").innerHTML = "Ridotto del 20%, il prezzo è di " + prezzo;
-    } else if (age.value=="3") {s
-        prezzo = chilo * costokm;
-        prezzo = prezzo - (40/100 * prezzo);
-        document.getElementById("costoBiglietto").innerHTML = "Ridotto del 40%, il prezzo è di " + prezzo.value;
+    if (age.value=="minorenne") {
+        prezzo = (parseInt(chilo.value) * costokm)
+        costofinale = prezzo - (20/100 *prezzo);
+        myName.innerHTML = user.value;
+        costoBiglietto.innerHTML = costofinale.toFixed(2) + "€";
+    } else if (age.value=="over65") {
+        prezzo = (parseInt(chilo.value) * costokm)
+        costofinale = prezzo - (40/100 *prezzo);
+        myName.innerHTML = user.value;
+        costoBiglietto.innerHTML = costofinale.toFixed(2) + "€";
     } else {
-        prezzo = chilo * costokm;
-        document.getElementById("costoBiglietto").innerHTML = "Prezzo intero, il costo del biglietto è di " + prezzo;
+        costofinale = (parseInt(chilo.value) * costokm)
+        myName.innerHTML = user.value;
+        costoBiglietto.innerHTML = costofinale.toFixed(2) + "€";
     }
     console.log("il prezzo è di: " + prezzo);
+    console.log("il costofinale è di: " + costofinale);
 });
-
