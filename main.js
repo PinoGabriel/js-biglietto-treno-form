bed1933c1800de4e7e99f7266592f250f654185a
PinoGabriel <pinogabriel01@gmail.com>
2023-10-28 16:06:23 +0200

    now age.value work

--- main.js
+++ main.js
@@ -5,21 +5,20 @@ let chilo =  document.getElementById("km");
 let age =  document.getElementById("eta");
 let costokm = 0.21
 let prezzo;
-let minorenne = document.getElementById("v2");
-let over65 = document.getElementById("v3");
+
 
 btn.addEventListener("click", function() {
     console.log("L'utente ha scritto: " + user.value)
     console.log("L'utente ha scritto: " + chilo.value)
-    console.log("L'utente ha scritto: " + age);
-    if (age = minorenne) {
+    console.log("L'utente ha scritto: " + age.value);
+    if (age.value=="2") {
         prezzo = chilo * costokm;
         prezzo = prezzo - (20/100 * prezzo);
         document.getElementById("costoBiglietto").innerHTML = "Ridotto del 20%, il prezzo è di " + prezzo;
-    } else if (age = over65) {
+    } else if (age.value=="3") {s
         prezzo = chilo * costokm;
         prezzo = prezzo - (40/100 * prezzo);
-        document.getElementById("costoBiglietto").innerHTML = "Ridotto del 40%, il prezzo è di " + prezzo;
+        document.getElementById("costoBiglietto").innerHTML = "Ridotto del 40%, il prezzo è di " + prezzo.value;
     } else {
         prezzo = chilo * costokm;
         document.getElementById("costoBiglietto").innerHTML = "Prezzo intero, il costo del biglietto è di " + prezzo;
